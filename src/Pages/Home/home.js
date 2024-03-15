52cf1040ce310fa9b33dcdc3a762fa9da1ac5509
PrayonFernando <prayonfernando@gmail.com>
2024-03-15 18:53:20 +0530

    setting up

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { TextField, Grid } from "@mui/material";
+import "./Home.css";
 
 function Home() {
   return;
-  <Grid></Grid>;
+  <Grid style={{ backgroundColor: "#05b0ec" }}>
+    {/* <header className="header">
+      <h1>My Header</h1>
+    </header> */}
+  </Grid>;
 }
 
 export default Home;
